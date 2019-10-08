2885e0fc84cb5134fd273977967a8844fa1ff5b8
blegal <blegal@users.noreply.github.com>
2019-10-08 15:32:28 +0200

    Update Gene.cpp

--- 1_SYSTEMC/Question_2/src/gene/Gene.cpp
+++ 1_SYSTEMC/Question_2/src/gene/Gene.cpp
@@ -17,9 +17,9 @@ void Gene::do_gen()           // process
 		d[1] = (int)(rand()%256);
 		d[2] = (int)(rand()%256);
 		cout << "Time = " << sc_time_stamp() << " => Valeurs emises : (R=" << d[0] << ", G=" << d[1] << ", B=" << d[2]  << ")" << endl;
-		s.write( d[2] );
-		s.write( d[1] );
 		s.write( d[0] );
+		s.write( d[1] );
+		s.write( d[2] );
 		wait( 10, SC_NS );
 	}
 }
